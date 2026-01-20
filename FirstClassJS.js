0d8b08b1798a3b73659d0210468fdb903433de0b
FabianoA-Araujo <fabiano.araujodeveloper@gmail.com>
2026-01-20 16:07:55 -0300

    first commit

---
+++ FirstClassJS.js
@@ -0,0 +1,12 @@
+// src/components/TesteComponente.js
+
+function TesteComponente() {
+  return (
+    <div>
+      <h1>Teste de Push no GitHub 🚀</h1>
+      <p>Se você está vendo isso, o componente foi criado com sucesso.</p>
+    </div>
+  );
+}
+
+export default TesteComponente;
